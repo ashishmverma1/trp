db5f39f35fb23abbdbf6d29e95e14d5e19c42ba9
Ashish Verma <ashishmverma1@gmail.com>
2015-12-18 22:08:40 +0530

    Created migrations.
    Migrations created: Users, Articles, Rating,  Comments

--- database/migrations/2015_12_18_154424_create_rating_table.php
+++ database/migrations/2015_12_18_154424_create_rating_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRatingTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rating', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('username');
+            $table->integer('post_id')->unsigned();
+            $table->tinyInteger('rating_value');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('rating');
     }
 }
