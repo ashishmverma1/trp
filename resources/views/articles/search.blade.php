99ded9af13045433e7a22e2a78e73c17d3fa2d72
Ashish Verma <ashishmverma1@gmail.com>
2015-12-29 00:08:52 +0530

    Styled articles search view

--- resources/views/articles/search.blade.php
+++ resources/views/articles/search.blade.php
@@ -7,35 +7,79 @@
 
 
 @section('css')
-@stop
-
-
-@section('js')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_articles.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
 @stop
 
 
 @section('content')
 
-    <h3>Search for: "{{ \Request::get('query') }}"</h3>
-    @if ($articles->count() == 1)
-        <p>{{ $articles->count() }} result</p>
-    @else
-        <p>{{ $articles->count() }} results</p>
-    @endif
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">Search for:</h1>
+        </div>
+        <div class="col-sm-12 search-result">
+            <p class="">'{{ \Request::get('query') }}'</p>
+            <p>
+                @if ($articles->count() == 1)
+                    {{ $articles->count() }} result found.
+                @else
+                    {{ $articles->count() }} results found.
+                @endif
+            </p>
+        </div>
+    </div>
 
+    <!-- Show results -->
     @foreach ($articles as $article)
 
-        <article>
-            <h3>
-                <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
-            </h3>
+        <div class="row">
+            <div class="article-preview-container col-sm-12 col-md-8 col-centered">
+                <div class="row article-preview-title">
+                    <a href="/articles/{{ $article->id }}">
+                        <div class="col-sm-12">
+                            <p>
+                                {{ $article->title }}
+                            </p>
+                        </div>
+                    </a>
+                </div>
 
-            <p>
-                {{ $article->body }}
-            </p>
-        </article>
+                <div class="row article-preview-body">
+                    <div class="col-sm-12">
+                        <p>
+                            {{ str_limit($article->body, $limit = 120,  $end = ' . . .') }}
+                        </p>
+                    </div>
+                </div>
 
-        <hr>
+                <div class="row article-preview-stats">
+                    <div class="col-sm-12 col-md-6">
+                        <p>
+                            <i class="fa fa-user"></i> Posted by:
+                            <a href="/users/{{ $article->user()->get()->first()->username }}">
+                                {{ $article->user()->get()->first()->username }}
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <p>
+                            <i class="fa fa-clock-o"></i> Posted on: {{ $article->created_at->toFormattedDateString() }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     @endforeach
 
