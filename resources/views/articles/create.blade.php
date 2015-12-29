d425093e16d3a37bc92bc5099fe9da1ac7674d13
Ashish Verma <ashishmverma1@gmail.com>
2015-12-29 21:08:01 +0530

    Styled create article view

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,24 +1,71 @@
 @extends('app')
 
+
 @section('title')
     New Article
 @stop
 
+
+@section('css')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_articles.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+@stop
+
+
 @section('content')
 
-    <form method="POST" accept-charset="UTF-8" action="/articles">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="text" name="title" value="{{ old('title') }}">
-        <input type="textarea" name="body" value="{{ old('body') }}">
-        <button type="submit">Post</button>
-    </form>
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">New Post</h1>
+        </div>
+    </div>
+
 
     @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="row">
+            <div class="col-sm-12 col-md-4 article-form-errors">
+                <p>Uh no, not there yet! You need to take care of  the following first:</p>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     @endif
+
+
+    <div class="row">
+        <div class="col-sm-12 col-md-8 article-form">
+            <form role="form" method="POST" accept-charset="UTF-8" action="/articles">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group title-section">
+                    <label for="title">Title</label>
+                    <input class="form-control" type="text" name="title" value="{{ old('title') }}"
+                            placeholder="Title for your new post">
+                </div>
+
+                <div class="form-group body-section">
+                    <label for="body">Body</label>
+                    <textarea  class="form-control" type="textarea" rows="10" name="body" placeholder="Body">{{ old('body') }}</textarea>
+                </div>
+
+                <button class="btn btn-primary btn-lg btn-block" type="submit">
+                    <i class="fa fa-send"></i> Post
+                </button>
+            </form>
+        </div>
+    </div>
 
 @stop
