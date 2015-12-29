043e196750edb8999ed751094a70dddd51859798
Ashish Verma <ashishmverma1@gmail.com>
2015-12-29 18:21:36 +0530

    Styled notifications view

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,33 +1,74 @@
 @extends('app')
 
+
 @section('title')
     Notifications
 @stop
 
+
+@section('css')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_articles.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+@stop
+
+
 @section('content')
 
-    <h1>Notifications</h1>
-    <p>{{ $notifications->where('read', 0)->count() }} new notifications.</p>
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">Notifications</h1>
+        </div>
+        <div class="col-sm-12 page-subheading">
+            <p>
+                {{ $notifications->where('read', 0)->count() }}
+                @if ($notifications->where('read', 0)->count() == 1)
+                    unread notification.
+                @else
+                    unread notifications.
+                @endif
+            </p>
+        </div>
+    </div>
 
-    @foreach ($notifications as $notification)
 
-        <a href="/notifications/{{ $notification->id }}"
-            <div>
-                <p>
-                    @if ($notification->read)
-                        Read:
-                    @else
-                        Unread:
-                    @endif
-                </p>
-                <p>
-                    New {{ $notification->type }}(s) on {{ $notification->article()->first()->title }}
-                </p>
+    <div class="notification-list-container">
+        @foreach ($notifications as $notification)
+            <div class="row">
+                <div class="col-sm-12 col-md-6 notification-container @if(!$notification->read) notification-unread @endif">
+                    <a href="/notifications/{{ $notification->id }}">
+                        <div>
+                            <p>
+                                New <b>{{ $notification->type }}</b>(s) on your post
+                                '<b>{{ $notification->article()->first()->title }}</b>'.
+                            </p>
+                            <div class="notification-stats">
+                                <span class="notification-read-indicator">
+                                    @if ($notification->read)
+                                        Read
+                                    @else
+                                        Unread
+                                    @endif
+                                </span>
+                                <i class="fa fa-dot-circle-o"></i>
+                                <span class="notification-timestamp">
+                                    {{ $notification->updated_at->diffForHumans() }}
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </a>
-
-        <hr>
-
-    @endforeach
+        @endforeach
+    </div>
 
 @stop
