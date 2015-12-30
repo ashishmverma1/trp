96290e3f9853ac8c65ee48c2faaf197dfa64fb33
Ashish Verma <ashishmverma1@gmail.com>
2015-12-31 01:12:06 +0530

    Styled user list view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,30 +1,66 @@
 @extends('app')
 
+
 @section('title')
     Authors
 @stop
 
+
+@section('css')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_articles.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+@stop
+
+
 @section('content')
-
-    <h1>Users</h1>
-
-    @foreach ($users as $user)
-
-        <div>
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">Authors</h1>
+        </div>
+        <div class="col-sm-12 page-subheading">
             <p>
-                <a href="/users/{{ $user->username }}">
-                    {{ $user->username }}
-                    @if ($user->numberOfArticles == 1)
-                        ({{ $user->numberOfArticles }} article)
-                    @else
-                        ({{ $user->numberOfArticles }} articles)
-                    @endif
-                </a>
+                {{ $users->count() }} authors
             </p>
         </div>
+    </div>
 
-        <hr>
 
-    @endforeach
+    <div class="row user-list-container">
+        @foreach ($users as $user)
+            <div class="col-sm-12 col-md-3 user-preview-col">
+                <a href="/users/{{ $user->username }}">
+                    <div class=" user-preview-container">
+                        <div class="user-preview-title">
+                            <p>
+                                {{ $user->username }}
+                            </p>
+                        </div>
+
+                        <div class="user-preview-stats">
+                            <p>
+                                <i class="fa fa-group"></i> Joined on: {{ $user->created_at->toFormattedDateString() }}
+                            </p>
+                            <p>
+                                <i class="fa fa-pencil-square"></i> Articles published: {{ $user->numberOfArticles }}
+                            </p>
+                            <p>
+                                <i class="fa fa-comments"></i> Comments posted: {{ $user->comments()->count() }}
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
 
 @stop
