6d40458dd396c9d8741b9367f397ce643925abaa
Ashish Verma <ashishmverma1@gmail.com>
2015-12-31 21:40:19 +0530

    Styled welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,76 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<!doctype HTML>
+<html lang="en">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<head>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        TRP
+    </title>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Titillium+Web:300'>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300">
+    <link rel="stylesheet" type="text/css" href="/css/welcome.css">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+</head>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+<body>
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-4 logo-container">
+                <img src="/img/logo.png" alt="Logo" height="120" width="240">
             </div>
         </div>
-    </body>
+
+        <div class="row">
+            <div class="col-sm-10 col-md-8 about-container">
+                <p>
+                    Lorem ipsum dolor sit amet, mea stet mucius quidam ad, sed denique abhorreant contentiones ad.
+                    Ius legendos invenire ne. Sea populo gloriatur ne, no sea atqui iracundia, ad reque essent reprimique has.
+                    Tritani torquatos his ut, at qui veri aliquam detracto. Pri aperiri luptatum ex.
+                    <br><br>
+                    Docendi euripidis adipiscing mei et, at nam posse mediocrem, epicuri nostrum sensibus eu nam.
+                    Est ei ponderum efficiendi. Ei amet democritum eam.
+                    Nostrum atomorum comprehensam ei est, id solum epicurei scaevola sit.
+                    Pri natum dignissim cu, mei an postea iisque temporibus. Mel no stet feugiat forensibus.
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-10 col-md-8 get-started-container">
+                <h3>Get Started!</h3>
+                <p>
+                    <a href="/auth/login" class="btn btn-primary btn-lg">
+                        <i class="fa fa-sign-in"></i> Login
+                    </a>
+                    &nbsp;&nbsp;
+                    <a href="/auth/register" class="btn btn-primary btn-lg">
+                        <i class="fa fa-user-plus"></i> Register
+                    </a>
+                    &nbsp;&nbsp; OR &nbsp;&nbsp;
+                    <a href="/articles" class="btn btn-primary btn-lg">
+                        <i class="fa fa-newspaper-o"></i> Read Articles
+                    </a>
+                </p>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
