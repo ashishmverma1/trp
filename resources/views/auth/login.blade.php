56e7fc923d2835e73b58efa70d602319e32416e0
Ashish Verma <ashishmverma1@gmail.com>
2015-12-31 18:51:11 +0530

    Styled login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,39 +1,73 @@
 @extends('app')
 
+
 @section('title')
     Login
 @stop
 
+
+@section('css')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_users.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+@stop
+
+
 @section('content')
 
-    <form method="POST" action="/auth/login">
-        {!! csrf_field() !!}
-
-        <div>
-            Pen Name
-            <input type="text" name="username" value="{{ old('username') }}">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">Login</h1>
         </div>
+    </div>
 
-        <div>
-            Password
-            <input type="password" name="password" id="password">
-        </div>
-
-        <div>
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
-
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
 
     @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="row">
+            <div class="col-sm-12 col-md-4 auth-form-errors">
+                <p>Error!</p>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     @endif
+
+
+    <div class="row">
+        <div class="col-sm-12 col-md-4 auth-form">
+            <form role="form" method="POST" accept-charset="UTF-8" action="/auth/login">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" value="{{ old('username') }}"
+                            placeholder="Pen Name">
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" id="password" placeholder="Password">
+                </div>
+
+                <div class="form-group">
+                    <input type="checkbox" name="remember" class="remember-me-checkbox"> Remember Me
+                </div>
+
+                <button class="btn btn-primary btn-lg btn-block" type="submit">
+                    <i class="fa fa-login"></i> Login
+                </button>
+            </form>
+        </div>
+    </div>
 
 @stop
