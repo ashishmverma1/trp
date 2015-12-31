cd7cced0849ea554a40e7d342a4d8c98453195e5
Ashish Verma <ashishmverma1@gmail.com>
2015-12-31 19:08:50 +0530

    Styled register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,77 @@
 @extends('app')
 
+
 @section('title')
     Register
 @stop
 
+
+@section('css')
+    <style>
+        body {
+            background-position: center;
+            background-image: url(/img/bg_users.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+@stop
+
+
 @section('content')
 
-    <form method="POST" action="/auth/register">
-        {!! csrf_field() !!}
-
-        <div>
-            Pen Name
-            <input type="text" name="username" value="{{ old('username') }}">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="page-heading">Register</h1>
         </div>
+    </div>
 
-        <div>
-            Password
-            <input type="password" name="password">
-        </div>
-
-        <div>
-            Confirm Password
-            <input type="password" name="password_confirmation">
-        </div>
-
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
 
     @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="row">
+            <div class="col-sm-12 col-md-4 auth-form-errors">
+                <p>Error!</p>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     @endif
+
+
+    <div class="row">
+        <div class="col-sm-12 col-md-4 auth-form">
+            <form role="form" method="POST" accept-charset="UTF-8" action="/auth/register">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" value="{{ old('username') }}"
+                            placeholder="Choose a pen name">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">
+                        Minimum <b>four</b> characters.<br>
+                        Choose your password wisely. You can change after login, but cannot reset it incase you forgot!
+                    </label>
+                    <input class="form-control" type="password" name="password" placeholder="Password">
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password_confirmation" placeholder="Confirm password">
+                </div>
+
+                <button class="btn btn-primary btn-lg btn-block" type="submit">
+                    <i class="fa fa-user-plus"></i> Register
+                </button>
+            </form>
+        </div>
+    </div>
 
 @stop
