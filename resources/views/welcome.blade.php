9beafac9a414a9a00317fa66cbaafe258f383fab
Ashish Verma <ashishmverma1@gmail.com>
2015-12-31 21:41:19 +0530

    Removed unneeded fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,6 @@
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Titillium+Web:300'>
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300">
     <link rel="stylesheet" type="text/css" href="/css/welcome.css">
 
